2391200d1205f2b2eea88b29c217e4ce895d9911
iser <constact@iser-labs.com>
2019-10-07 17:31:52 +0300

    Added tournament component

---
+++ src/components/TournamentComponent.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="tournament-layout">
+    <h6>{{ `${tournament.position}.${tournament.name}` }}</h6>
+    <div v-for="(match, index) in matchesOrdered" v-bind:key="index">
+      <!-- Match -->
+      <match-component :match="match"></match-component>
+    </div>
+  </div>
+</template>
+
+<script>
+import MatchComponent from "./MatchComponent";
+
+export default {
+  props: {
+    tournament: Object
+  },
+  computed: {
+    matchesOrdered() {
+      const matches = this.tournament.matches;
+      return matches.sort((a, b) => a.position - b.position);     
+    }
+  },
+  components: {
+    MatchComponent
+  }
+};
+</script>
+
+<style scoped>
+.tournament-layout {
+  display: grid;
+  grid-template-columns: 1fr;
+  gap: 0.2rem;
+}
+</style>
